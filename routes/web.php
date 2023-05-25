55390a22e9a08398c00eace438f567a621c4c7fa
Kriti M <macbookpro@macbooks-MacBook-Pro.local>
2023-05-25 21:46:09 +0700

    add feature authentication

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthenticationController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EmployeeController;
 
@@ -14,16 +15,18 @@ use App\Http\Controllers\EmployeeController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get('/pegawai' ,[EmployeeController::class, 'index'] )->name('pegawai');
 
-Route::get('/tambahpegawai' ,[EmployeeController::class, 'tambahpegawai'] )->name('tambahpegawai');
-Route::post('/insertdata' ,[EmployeeController::class, 'insertdata'] )->name('insertdata');
+Route::get('/login', [AuthenticationController::class, 'login'])->name('login')->middleware('guest');
+Route::get('/register', [AuthenticationController::class, 'register'])->name('register')->middleware('guest');
+Route::post('/auth/register', [AuthenticationController::class, 'userRegister']);
+Route::post('/auth/login', [AuthenticationController::class, 'userLogin']);
+Route::post('/auth/logout', [AuthenticationController::class, 'logout'])->middleware('auth');
 
-Route::get('/tampilkandata/{id}' ,[EmployeeController::class, 'tampilkandata'] )->name('tampilkandata');
-Route::post('/updatedata/{id}' ,[EmployeeController::class, 'updatedata'] )->name('updatedata');
+Route::get('/pegawai' ,[EmployeeController::class, 'index'] )->name('pegawai')->middleware('auth');
+Route::get('/tambahpegawai' ,[EmployeeController::class, 'tambahpegawai'] )->name('tambahpegawai')->middleware('auth');
+Route::post('/insertdata' ,[EmployeeController::class, 'insertdata'] )->name('insertdata')->middleware('auth');
 
-Route::get('/delete/{id}' ,[EmployeeController::class, 'delete'] )->name('delete');
+Route::get('/tampilkandata/{id}' ,[EmployeeController::class, 'tampilkandata'] )->name('tampilkandata')->middleware('auth');
+Route::post('/updatedata/{id}' ,[EmployeeController::class, 'updatedata'] )->name('updatedata')->middleware('auth');
+Route::get('/delete/{id}' ,[EmployeeController::class, 'delete'] )->name('delete')->middleware('auth');
