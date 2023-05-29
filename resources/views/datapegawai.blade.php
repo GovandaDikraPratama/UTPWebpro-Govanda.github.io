68a8c4629f29a9806a3206c6771c4ef72f8f8c4c
Kriti M <macbookpro@macbooks-MacBook-Pro.local>
2023-05-29 14:13:20 +0700

    Add new template for panel dashboard

--- resources/views/datapegawai.blade.php
+++ resources/views/datapegawai.blade.php
@@ -2,75 +2,68 @@
 
 @section('content')
 
-<div class="card bg-light border-0 shadow-lg">
-    <div class="card-body">
-      <h1 class="text-center mb-4 mt-4 text-dark display-4 fw-bold">DATA PEGAWAI</h1>
-    </div>
-  </div>
+<main id="main" class="main">
+
+    <div class="pagetitle">
+      <h1>Pegawai</h1>
+      <nav>
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item"><a href="/pegawai">Home</a></li>
+        </ol>
+      </nav>
+    </div><!-- End Page Title -->
+
+    <section class="section dashboard">
+        <div class="container mt-5">
+            <a href="/tambahpegawai" class="btn btn-outline-success">Tambah Data</a>
+            <div class="row">
+                @if ($message = Session::get('success'))
+                <div class="alert alert-success mt-4" role="alert">
+                    {{ $message }}
+                  </div>
+                @endif
+                <table class="table">
+                    <thead>
+                      <tr>
+                        <th scope="col">No</th>
+                        <th scope="col">Nama</th>
+                        <th scope="col">Foto</th>
+                        <th scope="col">Jenis Kelamin</th>
+                        <th scope="col">No Telepon</th>
+                        <th scope="col">History</th>
+                        <th scope="col">Aksi</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+
+                        @php
+                            $no = 1;
+                        @endphp
+
+                        @foreach ($data as $row)
+                        <tr>
+                            <th scope="row">{{ $no++ }}</th>
+                            <td>{{ $row->nama }}</td>
+                            <td>
+                                <img src="{{ asset('fotopegawai/' . $row->foto ) }}" alt="" style="width: 40px;">
+                            </td>
+                            <td>{{ $row->jenis_kelamin }}</td>
+                            <td>0{{ $row->notelpon }}</td>
+                            <td>{{ $row->created_at->format('D M Y')}}</td>
+                            <td>
+                                <a href="/tampilkandata/{{ $row->id  }}" class="btn btn-outline-warning">Edit</a>
+                                <a href="/delete/{{ $row->id  }}" class="btn btn-outline-danger">Delete</a>
+                            </td>
+                          </tr>
+                        @endforeach
 
 
-<div class="container mt-5">
-    <a href="/tambahpegawai" class="btn btn-outline-success">Tambah Data</a>
-    <div class="row">
-        @if ($message = Session::get('success'))
-        <div class="alert alert-success mt-4" role="alert">
-            {{ $message }}
-          </div>
-        @endif
-        <table class="table">
-            <thead>
-              <tr>
-                <th scope="col">No</th>
-                <th scope="col">Nama</th>
-                <th scope="col">Foto</th>
-                <th scope="col">Jenis Kelamin</th>
-                <th scope="col">No Telepon</th>
-                <th scope="col">History</th>
-                <th scope="col">Aksi</th>
-              </tr>
-            </thead>
-            <tbody>
+                    </tbody>
+                  </table>
+            </div>
+        </div>
+    </section>
 
-                @php
-                    $no = 1;
-                @endphp
-
-                @foreach ($data as $row)
-                <tr>
-                    <th scope="row">{{ $no++ }}</th>
-                    <td>{{ $row->nama }}</td>
-                    <td>
-                        <img src="{{ asset('fotopegawai/' . $row->foto ) }}" alt="" style="width: 40px;">
-                    </td>
-                    <td>{{ $row->jenis_kelamin }}</td>
-                    <td>0{{ $row->notelpon }}</td>
-                    <td>{{ $row->created_at->format('D M Y')}}</td>
-                    <td>
-                        <a href="/tampilkandata/{{ $row->id  }}" class="btn btn-outline-warning">Edit</a>
-                        <a href="/delete/{{ $row->id  }}" class="btn btn-outline-danger">Delete</a>
-                    </td>
-                  </tr>
-                @endforeach
-
-
-            </tbody>
-          </table>
-    </div>
-</div>
-@endsection
-
-@section('footer')
-<footer>
-  <div class="card text-center mt-5">
-     <div class="card-header">
-        UJIAN TENGAH SEMESTER
-     </div>
-     <div class="card-body">
-        <h5 class="card-title">WEB PROGRAMMING</h5>
-     </div>
-     <div class="card-footer text-muted">
-        copyright.2023
-     </div>
-  </div>
-</footer>
+</main>
+<!-- End #main -->
 @endsection
--- resources/views/tambahdata.blade.php
+++ resources/views/tambahdata.blade.php
@@ -1,83 +1,60 @@
 @extends('layout')
 
 @section('content')
-<div class="card bg-light border-0 shadow-lg">
-  <div class="card-body">
-    <h1 class="text-center mb-4 mt-4 text-dark display-4 fw-bold">TAMBAH DATA PEGAWAI</h1>
-  </div>
-</div>
 
+<main id="main" class="main">
 
-<div class="container">
+    <div class="pagetitle">
+      <h1>Pagawai</h1>
+      <nav>
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item"><a href="/pegawai">Home</a></li>
+          <li class="breadcrumb-item active">Tambah Data</li>
+        </ol>
+      </nav>
+    </div><!-- End Page Title -->
 
-  <div class="row justify-content-center mt-4">
-      <div class="col-8">
-          <div class="card">
-              <div class="card-body">
-                  <form action="/insertdata" method="POST" enctype="multipart/form-data">
-                      @csrf
-                      <div class="mb-3">
-                        <label for="exampleInputEmail1" class="form-label">Nama Lengkap</label>
-                        <input type="text" name="nama" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                      </div>
-                      <div class="mb-3">
-                        <label for="exampleInputEmail1" class="form-label">Jenis Kelamin</label>
-                        <select class="form-select" name="jenis_kelamin" aria-label="Default select example">
-                          <option selected>Pilih Jenis Kelamin</option>
-                          <option value="cowo">cowo</option>
-                          <option value="cewe">cewe</option>
-                        </select>
-                      </div>
-                      <div class="mb-3">
-                        <label for="exampleInputEmail1" class="form-label">No Telepon</label>
-                        <input type="number" name="notelpon" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                      </div>
+    <section class="section dashboard">
+        <div class="container">
 
-                      <div class="mb-3">
-                        <label for="exampleInputEmail1" class="form-label">Masukkan Foto</label>
-                        <input type="file" name="foto" class="form-control" >
-                      </div>
+            <div class="row justify-content-center mt-4">
+                <div class="col-8">
+                    <div class="card">
+                        <div class="card-body p-5">
+                            <form action="/insertdata" method="POST" enctype="multipart/form-data">
+                                @csrf
+                                <div class="mb-3">
+                                  <label for="exampleInputEmail1" class="form-label">Nama Lengkap</label>
+                                  <input type="text" name="nama" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                                </div>
+                                <div class="mb-3">
+                                  <label for="exampleInputEmail1" class="form-label">Jenis Kelamin</label>
+                                  <select class="form-select" name="jenis_kelamin" aria-label="Default select example">
+                                    <option selected>Pilih Jenis Kelamin</option>
+                                    <option value="cowo">cowo</option>
+                                    <option value="cewe">cewe</option>
+                                  </select>
+                                </div>
+                                <div class="mb-3">
+                                  <label for="exampleInputEmail1" class="form-label">No Telepon</label>
+                                  <input type="number" name="notelpon" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                                </div>
 
-                      <button type="submit" class="btn btn-primary">Submit</button>
-                    </form>
-              </div>
+                                <div class="mb-3">
+                                  <label for="exampleInputEmail1" class="form-label">Masukkan Foto</label>
+                                  <input type="file" name="foto" class="form-control" >
+                                </div>
+
+                                <button type="submit" class="btn btn-primary">Submit</button>
+                              </form>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
           </div>
-      </div>
+    </section>
 
-  </div>
-</div>
+</main>
+<!-- End #main -->
 @endsection
-
-
-
-
-    <!-- Optional JavaScript; choose one of the two! -->
-
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-    -->
-  </body>
-
-  @section('footer')
-  <footer>
-    <div class="card text-center mt-5">
-       <div class="card-header">
-          UJIAN TENGAH SEMESTER
-       </div>
-       <div class="card-body">
-          <h5 class="card-title">WEB PROGRAMMING</h5>
-          <p class="card-text">GOVANDA DIKRA PRATAMA_065120026</p>
-       </div>
-       <div class="card-footer text-muted">
-          copyright.2023
-       </div>
-    </div>
-  </footer>
-  @endsection
-
-</html>
